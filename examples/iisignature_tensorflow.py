derivative wrt the fixed time

examples/iisignature_tensorflow.py
@@ -23,6 +23,10 @@ def _sigJoinGradImp(g,x,y,m):
     o= iisignature.sigjoinbackprop(g,x,y,m)
     return o[0],o[1],_zero
 
+def _sigJoinGradImpFixed(g,x,y,m,fixedlast):
+    o= iisignature.sigjoinbackprop(g,x,y,m,fixedlast)
+    return o[0],o[1],_zero,np.array(o[2],dtype="float32")
+
 def _sigGrad(op, grad):
     return tf.py_func(_sigGradImp,[grad]+list(op.inputs),
                       [tf.float32]*2, name="SigGrad")
@@ -35,6 +39,10 @@ def _sigJoinGrad(op, grad):
     return tf.py_func(_sigJoinGradImp,[grad]+list(op.inputs),
                       [tf.float32]*3, name="SigJoinGrad")
 
+def _sigJoinGradFixed(op, grad):
+    return tf.py_func(_sigJoinGradFixedImp,[grad]+list(op.inputs),
+                      [tf.float32]*4, name="SigJoinGradFixed")
+
 def _sigImp(x,m):
     return iisignature.sig(x,m)
 
@@ -43,6 +51,8 @@ def _sigScaleImp(x,y,m):
 
 def _sigJoinImp(x,y,m):
     return iisignature.sigjoin(x,y,m)
+def _sigJoinFixedImp(x,y,m,fixedLast):
+    return iisignature.sigjoin(x,y,m,fixedLast)
 
 def Sig(x, m):
     rnd_name = 'PyFuncGrad' + str(np.random.randint(0, 1E+8))
@@ -58,12 +68,18 @@ def SigScale(x, y, m):
     with g.gradient_override_map({"PyFunc":rnd_name}):
         return tf.py_func(_sigScaleImp, [x,y,m], tf.float32, name="SigScale") 
     
-def SigJoin(x,y,m):
+def SigJoin(x,y,m,fixedLast=None):
     rnd_name = 'PyFuncGrad' + str(np.random.randint(0, 1E+8))
-    tf.RegisterGradient(rnd_name)(_sigJoinGrad)
-    g=tf.get_default_graph()
-    with g.gradient_override_map({"PyFunc":rnd_name}):
-        return tf.py_func(_sigJoinImp, [x,y,m], tf.float32, name="SigJoin")
+    if fixedLast is None:
+        tf.RegisterGradient(rnd_name)(_sigJoinGrad)
+        g=tf.get_default_graph()
+        with g.gradient_override_map({"PyFunc":rnd_name}):
+            return tf.py_func(_sigJoinImp, [x,y,m], tf.float32, name="SigJoin")
+    else:
+        tf.RegisterGradient(rnd_name)(_sigJoinGradFixed)
+        g=tf.get_default_graph()
+        with g.gradient_override_map({"PyFunc":rnd_name}):
+            return tf.py_func(_sigJoinFixedImp, [x,y,m,fixedLast], tf.float32, name="SigJoin")
 
 #https://stackoverflow.com/questions/37924071/tensorflow-writing-an-op-in-python
 #https://gist.github.com/harpone/3453185b41d8d985356cbe5e57d67342

examples/iisignature_theano.py
@@ -86,11 +86,11 @@ class Sig_op(theano.Op):
 Sig = Sig_op()
 
 #This is a theano Op which wraps iisignature.sigjoinbackprop .
-#It has two outputs - there is nothing strange about this.
+#It has multiple outputs.
 class SigJoinGrad_op(theano.Op):
     __props__=()
     def infer_shape(self,node,shapes):
-        return [shapes[1],shapes[2]]
+        return [shapes[1],shapes[2], None]
     def make_node(self,s,x,y,m,fixed):
         s=theano.tensor.as_tensor_variable(s)
         x=theano.tensor.as_tensor_variable(x)
@@ -99,7 +99,7 @@ class SigJoinGrad_op(theano.Op):
         fixed=theano.tensor.as_tensor_variable(fixed)
         outT=theano.tensor.TensorType("float32",[False]*(x.ndim))
         return theano.Apply(self,inputs=[s,x,y,m,fixed],
-                            outputs=[outT(), outT()])
+                            outputs=[outT(), outT(), theano.tensor.dscalar()])
     def perform(self,node,inputs_storage,out):
         s=inputs_storage[0]
         x=inputs_storage[1]
@@ -120,6 +120,7 @@ class SigJoinGrad_op(theano.Op):
                 raise RuntimeError("nan in output")
         out[0][0]=o[0]
         out[1][0]=o[1]
+        out[2][0]=np.array(0.0 if np.isnan(fixed) else o[2], dtype="float64")
 SigJoinGrad = SigJoinGrad_op()
 
 #This is a theano Op which wraps iisignature.sigjoin .
@@ -152,7 +153,7 @@ class SigJoin_op(theano.Op):
         gg = SigJoinGrad(g[0],inputs[0],inputs[1],inputs[2],inputs[3])
         return [gg[0],gg[1],
                 theano.gradient.grad_undefined(self,2,inputs[2]),
-                theano.gradient.grad_not_implemented(self,3,inputs[3])]
+                gg[2]]
 SigJoin = SigJoin_op()
 
 #This is a theano Op which wraps iisignature.sigscalebackprop .