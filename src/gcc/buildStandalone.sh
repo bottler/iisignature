164c97d7628d51d937f6dd3ea6bc832c0b865b62
jeremy <bottler@github.com>
2017-08-09 15:57:38 +0100

    update example

--- src/gcc/buildStandalone.sh
+++ src/gcc/buildStandalone.sh
@@ -1,1 +1,7 @@
-clang++-3.6 -g -O2 -I.. -std=c++11 -Wall -Wextra -Wno-unused-parameter standalone.cpp 
+ARGS=
+ARGS=-m32
+COMPILER=clang++-3.6
+COMPILER=clang++-3.8
+COMPILER=g++
+
+$COMPILER $ARGS -g -O2 -I.. -std=c++11 -Wall -Wextra -Wno-unused-parameter standalone.cpp 
--- src/gcc/standalone.cpp
+++ src/gcc/standalone.cpp
@@ -13,9 +13,21 @@ void testFreq(){
   BasisElt lw5(5);
   BasisElt lw6(6);
   BasisElt l (lw4, BasisElt(lw4, BasisElt(BasisElt(lw6,lw4), BasisElt(lw4, BasisElt(lw5,lw6)))));
-  auto f= getLetterFrequencies(&l);
+  auto f= IISignature_algebra::getLetterFrequencies(&l);
   for(size_t i : f)
     std::cout <<i<<"\n";
+}
+
+void logSigCalculation(){
+  WantedMethods wantedmethods;
+  LogSigFunction lsf(LieBasis::Lyndon);
+  vector<double> a{0,1}, b{1,0}, sig{0,1,0,0,0,0,0,0};
+  WantedMethods wm;
+  wm.m_compiled_bch = wm.m_simple_bch = wm.m_expanded = wm.m_log_of_signature = false; 
+  wm.m_compiled_bch = true;
+  makeLogSigFunction(2,3,lsf,wm,doNothing);
+  lsf.m_f->go(sig.data(),b.data());
+
 }
 
 int main(){
@@ -26,15 +38,9 @@ int main(){
   auto s = new std::string(std::istreambuf_iterator<char>(stream), {});
   g_bchLyndon20_dat = s->c_str();
 
-#if 0
-  auto aa = ReadBCH::read();
-  const auto &a = aa.m_rows;
-  std::cout<<a.size()<<"\n";
-  auto r = a.back();
-  std::cout<<r.m_left<<" "<<r.m_right<<" "<<r.m_coeff<<"\n";
-  for(int i : aa.m_totalLengths)
-    std::cout<<i<<" ";
-#endif
+
+  logSigCalculation();
+  
   //calcFla(2,4,doNothing);
   LogSigFunction lsf(LieBasis::Lyndon);
   vector<double> a{0,1}, b{1,0}, sig{0,1,0,0,0,0,0,0};
