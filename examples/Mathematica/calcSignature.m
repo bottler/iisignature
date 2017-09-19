8a571b26d3ec3bfc0f5b44e52c961b0358a4c8c0
Jeremy <bottler@github.com>
2017-09-19 16:53:22 +0100

    minor

--- examples/Mathematica/calcSignature.m
+++ examples/Mathematica/calcSignature.m
@@ -2,13 +2,11 @@
 and m is a positive integer,
 then Sig[p,m] is the signature of p up to level m excluding the initial 1, given as a list of nested
 lists. *)
-Sig[p_, m_] := 
+Sig[p, m_] := 
  Module[{StraightSig, Chen}, 
   StraightSig[displacement_] := 
-   First /@ 
-    NestList[{Outer[Times, 
-         displacement, #[[1]]]/(#[[2]] + 1), #[[2]] + 
-        1} &, {displacement, 1}, m - 1]; 
+   FoldList[Outer[Times, displacement/#2, #1] &, displacement, 
+    Range[2, m]]; 
   Chen[d1_, d2_] := 
    Join[{d1[[1]] + d2[[1]]}, 
     Table[d1[[level]] + d2[[level]] + 
