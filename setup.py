b9a512fb664d7caa01b368ebd6dff9361f646b11
jeremy <bottler@github.com>
2017-09-20 16:41:13 +0100

    bump version to 0.21

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, Extension
 import numpy, os
 
-version = "0.20"
+version = "0.21"
 headers = ["bch","calcSignature","logsig","logSigLength","makeCompiledFunction",
             "rotationalInvariants","readBCHCoeffs"]
 
